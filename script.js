2ef692e6e7caf29b62b9d657ea4c6ec4414c0242
Atharva-Deshmukh1 <146633061+Atharva-Deshmukh1@users.noreply.github.com>
2023-10-21 16:17:27 +0530

    Update script

--- script.js
+++ script.js
@@ -11,7 +11,36 @@ const pointer = document.getElementById(`pointer`);
 window.addEventListener ("mousemove",(e)=>{
     const x =e.pageX;
     const y =e.pageY;
+
     
-    pointer.style.left=`${x}px`;
-    pointer.style.top=`${y}px`;
+        pointer.style.left=`${x}px`;
+        pointer.style.top=`${y}px`;
     });
+
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:"main",
+    markers:false,
+    start:"70% 50%",
+    end:"180% 50%",
+    scrub:2,
+    duration:1,
+    pin:true,
+}});
+
+tl
+.to(".container",{
+    width:"100%",
+    height:"100%"
+})
+gsap.from(".title",{
+    y:-100,
+    duration:1,
+    delay:1,
+    opacity:0,
+})
+gsap.from(".nav",{
+    y:-100,
+    duration:1,
+    delay:1,
+    opacity:0,
+})
